1570720b0d46e7d27263c7b584163308151ad215
Chana Kupetz <chanakupetz@gmail.com>
2020-07-17 03:18:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,6 +5,6 @@ def start_game(player_1, player_2)
   return "Hello #{player_1} & #{player_2}"
 end
 
-def play_game(player_1, player2)
+def play_game(player_1, player_2)
   return "#{player_1} is better than #{player_2}"
 end
